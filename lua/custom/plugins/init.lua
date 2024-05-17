bcd7a17f18dd1d8ce8c0ec52bbe3663671849861
christopher <christophervos522@gmail.com>
2024-05-17 07:00:00 +0200

    added tabnine and notify

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -26,17 +26,42 @@ return {
       -- local harpoon = require('harpoon').setup()
 
       vim.keymap.set('n', '<leader>a', function()
-        local harp = require 'harpoon.mark'
-
+        local harp
+        -- harpoon.ui:toggle_quick_menu(harpoon:list())
         harp.add_file()
-        -- harpoon:list():add()
       end)
       vim.keymap.set('n', '<C-e>', function()
         local harp = require 'harpoon.ui'
 
         harp.toggle_quick_menu()
-        -- harpoon.ui:toggle_quick_menu(harpoon:list())
       end)
     end,
   },
+
+  -- notify plugin
+  {
+    'rcarriga/nvim-notify',
+    config = function()
+      vim.notify = require 'notify'
+    end,
+  },
+
+  --  tabnine plaugin
+  {
+    'codota/tabnine-nvim',
+    build = './dl_binaries.sh',
+    config = function()
+      require('tabnine').setup {
+        disable_auto_comment = true,
+        accept_keymap = '<Tab>',
+        dismiss_keymap = '<C-]>',
+        debounce_ms = 800,
+        suggestion_color = { gui = '#808080', cterm = 244 },
+        exclude_filetypes = { 'TelescopePrompt', 'NvimTree' },
+        log_file_path = nil, -- absolute path to Tabnine log file
+      }
+    end,
+  },
+
+  -- stuff
 }
